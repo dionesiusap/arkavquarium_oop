46c02a31568c12f1167225405e149a8cb6678819
dionesiusap <dionesiusap@gmail.com>
2018-03-15 11:56:54 +0700

    added Aquarium class

--- libs/objects/Aquarium/Aquarium.hpp
+++ libs/objects/Aquarium/Aquarium.hpp
@@ -12,17 +12,24 @@ class Aquarium {
 public:
     // CTOR
     Aquarium();
+    Aquarium(int _width, int _height);
+    Aquarium(const Aquarium& aquarium);
+
+    // GETTER SETTER
+    
 
     // METHODS
-    void AddFishFromFish(const Fish& _fish);
-    void AddCoinFromCoins(const Coin& coin);
-    void AddFoodfromFoods(const Food& food);
+    friend void AddFishFromFish(const Fish& _fish);
+    friend void AddCoinFromCoins(const Coin &coin);
+    friend void AddFoodfromFoods(const Food &food);
 
-    void DeleteFishFromFish(const Fish& _fish);
-    void DeleteCoinFromCoins(const Coin& coin);
-    void DeleteFoodfromFoods(const Food& food);
-    
-private:
+    friend void DeleteFishFromFish(const Fish &_fish);
+    friend void DeleteCoinFromCoins(const Coin &coin);
+    friend void DeleteFoodfromFoods(const Food &food);
+
+  private:
+    int width;
+    int height;
     LinkedList<Fish> fish;
     LinkedList<Coin> coins;
     LinkedList<Food> foods;
